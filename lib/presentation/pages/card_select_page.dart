235087b5c6b3d1a03db0d6e3f6af55f6d530b560
jutole <jutole@jutoleui-Macmini.local>
2025-02-11 01:35:14 +0900

    카드 위젯화, 상세 다이얼로그 작업

--- lib/presentation/pages/card_select_page.dart
+++ lib/presentation/pages/card_select_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pocket_trade/core/theme/app_text_styles.dart';
-import 'package:pocket_trade/data/models/card_model.dart';
-import 'package:pocket_trade/domain/services/card_service.dart';
 import 'package:pocket_trade/presentation/base/base_scaffold.dart';
+import 'package:pocket_trade/presentation/dialogs/card_detail_dialog.dart';
 import 'package:pocket_trade/presentation/providers/card_provider.dart';
 import 'package:pocket_trade/presentation/viewmodels/card_select_viewmodel.dart';
+import 'package:pocket_trade/presentation/widgets/card_item.dart';
 import 'package:provider/provider.dart';
 
 class CardSelectPage extends StatelessWidget {
@@ -51,12 +51,54 @@ class CardSelectPage extends StatelessWidget {
                           ),
                           itemCount: selectedCards.data!.length,
                           itemBuilder: (context, index) {
-                            return Image.network(
-                              selectedCards.data![index].imageUrl,
-                              width: double.infinity,
-                              height: double.infinity,
-                            );
+                            return CardItem(card: selectedCards.data![index]);
                           },
+                        );
+                      }
+                      if (selectedCards.isError) {
+                        return const Center(
+                          child: Text("오류가 발생했습니다."),
+                        );
+                      }
+                      return const SizedBox.shrink();
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    "검색한 포켓몬",
+                    style: AppTextStyle.style18Bold,
+                  ),
+                  const SizedBox(height: 8),
+                  ChangeNotifierProvider.value(
+                    value: context.read<CardSelectViewModel>().selectedCards,
+                    builder: (context, child) {
+                      final selectedCards = context
+                          .select((CardProvider provider) => provider.cards);
+
+                      if (selectedCards.isLoading) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      if (selectedCards.isSuccess) {
+                        return Expanded(
+                          child: GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              childAspectRatio: 0.715,
+                              crossAxisSpacing: 8,
+                            ),
+                            itemCount: selectedCards.data!.length,
+                            itemBuilder: (context, index) {
+                              return CardItem(
+                                card: selectedCards.data![index],
+                                onTap: (card) {
+                                  CardDetailDialog(card: card).show(context);
+                                },
+                              );
+                            },
+                          ),
                         );
                       }
                       if (selectedCards.isError) {
